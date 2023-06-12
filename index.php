ae6f583688f9b93250c407c452def0db6ff8d95a
mevine76 <veeren@free.fr>
2023-06-12 13:06:24 +0200

    conditions ok

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+// $age = 35;
+// if ($age >= 18) {
+//     echo "vous êtes majeur";
+// }
+// else {
+//     echo "vous êtes mineur";
+// }
+
+// $isEasy = false;
+// if($isEasy == true) {
+//     echo "c'est facile";
+// }
+// else {
+//     echo "c'est difficile";
+// }
+
+// $age = 19;
+// $gender = "Homme";
+
+// if($age >= 18 && $gender == "Homme") {
+//     echo "vous êtes un homme et majeur";
+// }
+// else {
+//     echo "Vous êtes un Homme et mineur";
+// }
+
+// $age = 17;
+// $gender = "Femme";
+
+// if($gender == "Femme" && $age >= 18) {
+//     echo "vous êtes une Femme et majeur";
+// }
+// else {
+//     echo "vous êtes une Femme et mineure";
+// }
+
+
+// $magnitude = 9;
+
+// switch ($magnitude) {
+//     case "1" :
+//         echo "micro-seïsme impossible à ressentir";
+//         break;
+//     case "2" :
+//         echo "micro-seïsme impossible à ressentir mais enregistrable par les sismomètres";
+//         break;
+//     case "3" :
+//         echo "Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti";
+//         break;
+//     case "4" :
+//         echo "Seïsme capable de faire bouger des objets mais ne causent généralement pas dégats";
+//         break;
+//     case "6" :
+//         echo "Fort séisme capable d'engendrer des destructions majeures sur une large distance (180 km) autour de l'épicentre";
+//         break;
+//     case "7" :
+//         echo "Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance";
+//         break;
+//     case 8 :
+//         echo "Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres";
+//         break;
+//     case "9" :
+//         echo "Séisme capable de tout détruire sur une très vaste zone";
+//         break;
+//     default :
+//         echo "tout est calme";
+// }
+
+// $gender = "Homme";
+// $job = "développeuse";
+
+
+// if($gender != "Homme") {
+//     echo "c'est" . " " . "une" . " " . $job . " " . "!!!";
+// }
+// else {
+//     echo "c'est" . " " . "un" . " " . "développeur" . " " . "!!!";
+// }
+
+// $age = 17;
+// if($age >= 18) {
+//     echo "Tu es majeur";
+// }
+// else {
+//     echo "Tu n'es pas majeur";
+// }
+
+
+// $isOk = false;
+// if($isOk == false) {
+//     echo "c'est pas bon !!";
+// }
+// else {
+//     echo "c'est ok !!";
+// }
+
+
+// $isOk = false;
+// if($isOk == true) {
+//     echo "c'est ok !!";
+// }
+// else {
+//     echo "c'est pas bon !!";
+// }
+
+  
+?>
+
+
+
+
+
+    
+</body>
+</html>
